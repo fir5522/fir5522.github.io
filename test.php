ae6a20bdd288c9765890a9d6a64ac3083c7c1e95
fir5522 <fir5522@dcccd.edu>
2016-04-28 10:18:55 -0500

    Upload my local files to github
    All the files from web2

---
+++ test.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+echo 'Current PHP version: ' . phpversion();
+?>
+
+</body>
+</html>
